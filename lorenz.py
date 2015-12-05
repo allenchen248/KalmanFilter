a349e9edffbe90cc73968d890ebcc2d9f30e5e33
Allen Chen <allenchen248@gmail.com>
2015-12-05 01:06:47 -0500

    Added MCMC Testing

---
+++ lorenz.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+def lorentz_func(t, xs, sigma, rho, beta, **kwargs):
+	"""
+	Given [x, y, z] and parameters, returns [dx/dt, dy/dt, dz/dt]
+	with the given parameters.
+	"""
+	return np.array([sigma*(xs[1]-xs[0]), xs[0]*(rho-xs[2])-xs[1], xs[0]*xs[1] - beta*xs[2]])
+
+def keep_run(t, xs, tf=1, **kwargs):
+	return t < tf
+
+def rk4(h, xs0=[1,1,1], keep_running=keep_run, f=lorentz_func, **kwargs):
+	"""
+	Implements the classical fourth-order Runge-Kutta method,
+	and stops at end condition keep_running.
+
+	Evaluates the function y' = f() [f is the argument]
+	"""
+	output = [(0,np.array([float(x) for x in xs0]))]
+	h = float(h)
+	t = 0
+	while keep_running(t, output[-1][1], **kwargs):
+		xs = output[-1][1]
+		k1 = f(t, xs, **kwargs)
+		k2 = f(t+h/2, xs+h*k1/2, **kwargs)
+		k3 = f(t+h/2, xs+h*k2/2, **kwargs)
+		k4 = f(t+h, xs+h*k3, **kwargs)
+		t += h
+		output.append((t, xs+h*(k1+2*k2+2*k3+k4)/6))
+
+	return output
+
+def plot_lorentz(output):
+	"""
+	Plots the lorentz function output!
+	"""
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	coords = zip(*[o[1] for o in output])
+	ax.plot(coords[0], coords[1], coords[2])
+	plt.show(block=False)
+
+class LorentzAttractor:
+	def __init__(self, xs0=[.1,0.,0.], sigma=10., rho=28., beta=8/3., tf=50., 
+			h=0.005, f=lorentz_func):
+		rk4_out = rk4(h, xs0, sigma=float(sigma), rho=float(rho), beta=float(beta), tf=float(tf), f=f)
+		self.f = f
+		self.tf = tf
+		self.la_params = {'sigma':sigma, 'rho':rho, 'beta':beta}
+		self.ts = [o[0] for o in rk4_out]
+		self.xs = {o[0]:o[1] for o in rk4_out}
+
+	
+
+	def __getitem__(self, tnew):
+		"""
+		If xs at that time doesn't exist, run a step of RK4 in order to
+		generate what the x value should be there.
+		"""
+		if (tnew > self.tf) or (tnew <= 0):
+			raise ValueError("Time Isn't Within Bounds")
+
+		if tnew in self.xs:
+			return self.xs[tnew]
+
+		told = self.ts[np.searchsorted(self.ts, tnew)]
+		h = float(tnew-told)
+		x = self.xs[told]
+		k1 = self.f(told, x, **self.la_params)
+		k2 = self.f(told+h/2, x+h*k1/2, **self.la_params)
+		k3 = self.f(told+h/2, x+h*k2/2, **self.la_params)
+		k4 = self.f(told+h, x+h*k3, **self.la_params)
+		return x+h*(k1+2*k2+2*k3+k4)/6
+
+	def plot(self):
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection='3d')
+		coords = zip(*[self.xs[t] for t in self.ts])
+		ax.plot(coords[0], coords[1], coords[2])
+		plt.show(block=False)
--- mcmc/__init__.py
+++ mcmc/__init__.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.random as random
 
 class Uniform:
-	def __init__(self, a, b, temperature=0.5):
+	def __init__(self, a, b, temperature=0.1):
 		if b <= a:
 			raise ValueError("Can't have a uniform with lower bound higher than upper bound.")
 		self.a = a
@@ -10,12 +10,18 @@ class Uniform:
 		self.temp = temperature*(b-a)
 		self.value = random.uniform(self.a, self.b)
 
+	def __str__(self):
+		return "Unif(%.2f, %.2f) with value %.4f" % (self.a, self.b, self.value)
+
+	def __repr__(self):
+		return self.__str__() 
+
 	def generate(self):
 		return self.value
 
 	def resample(self):
 		# Do not store in self.value because that involves a pointer dereference
-		newval = random.uniform(self.value-self.temp, self.value+self.temp)
+		newval = random.triangular(self.value-self.temp, self.value, self.value+self.temp)
 		while (newval > self.b) or (newval < self.a):
 			newval = random.uniform(self.value-self.temp, self.value+self.temp)
 
@@ -25,8 +31,29 @@ class Uniform:
 	def prior_loglik(self):
 		return -1*np.log(self.b-self.a)
 
+class Bernoulli:
+	def __init__(self, p):
+		self.p = p
+		self.value = int(random.uniform(0,1) < self.p)
+
+	def __str__(self):
+		return "Bern(%.4f) with value %d" % (self.p, self.value)
+
+	def __repr__(self):
+		return self.__str__()
+
+	def resample(self):
+		self.value = int(random.uniform(0,1) < self.p)
+		return self.value
+
+	def prior_loglik(self):
+		if self.value == 1:
+			return np.log(self.p)
+		else:
+			return np.log(1-self.p)
+
 class Normal:
-	def __init__(self, mu, sigma, temperature=0.5):
+	def __init__(self, mu, sigma, temperature=0.2):
 		if sigma <= 0:
 			raise ValueError("Can't have a gaussian with standard deviation < 0")
 
@@ -34,6 +61,12 @@ class Normal:
 		self.sigma = sigma
 		self.temp = temperature*sigma
 		self.value = random.normal(mu, sigma)
+
+	def __str__(self):
+		return "N(%.2f, %.2f) with value %.4f" % (self.mu, self.sigma, self.value)
+
+	def __repr__(self):
+		return self.__str__()
 
 	def generate(self):
 		return self.value
@@ -51,13 +84,15 @@ class MCMC:
 		self.prev = {}
 		self.prev_loglik = None
 		self.changed = None
+		self.path = []
 
 	def generate_rv(self, name, rv_func):
-		if name not in self.prev:
-			self.rvs[name] = rv_func()
-		else:
-			self.rvs[name] = self.prev[name]
+		#if name not in self.prev:
+		#	self.rvs[name] = rv_func()
+		#else:
+		#	self.rvs[name] = self.prev[name]
 
+		self.rvs[name] = rv_func()
 		return self.rvs[name].value
 
 	def uniform(self, name, a=0, b=1, **kwargs):
@@ -66,7 +101,11 @@ class MCMC:
 	def normal(self, name, mu=0, sigma=1, **kwargs):
 		return self.generate_rv(name, lambda: Normal(mu, sigma, **kwargs))
 
+	def bernoulli(self, name, p=0.5, **kwargs):
+		return self.generate_rv(name, lambda: Bernoulli(p, **kwargs))
+
 	def accept(self, val):
+		self.path.append(self.prev)
 		self.prev = self.rvs
 		self.resample()
 
@@ -100,10 +139,12 @@ class MCMC:
 		accept_ratio = min(1, np.exp(prior_ratio + cur_ratio))
 
 		if random.uniform(0,1) < accept_ratio:
+			print accept_ratio, prior_ratio, cur_ratio
+			#print "ACCEPT"
 			return self.accept(loglik_val)
 
+		#print "REJECT"
 		return self.reject()
-
 
 
 
